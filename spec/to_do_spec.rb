4909120d8ace2997cf9a66bf2ed5fd940bb9e609
Alex Altieri & Jake Glassman <student@epicodus.com>
2015-08-17 11:00:05 -0700

    Add files for List class, pass specs for unit testing

--- spec/to_do_spec.rb
+++ spec/to_do_spec.rb
@@ -1,28 +1,18 @@
-require('rspec')
-require('to_do')
-require('pg')
-
-DB = PG.connect({:dbname => 'to_do_test'})
-
-RSpec.configure do |config|
-  config.after(:each) do
-    DB.exec("DELETE FROM tasks *;")
-  end
-end
+require('spec_helper')
 
 describe(Task) do
 
   describe('#==') do
     it('is the same task if it has the same description') do
-      task1 = Task.new({:description => 'learn SQL'})
-      task2 = Task.new({:description => 'learn SQL'})
+      task1 = Task.new({:description => 'learn SQL', :list_id => 1})
+      task2 = Task.new({:description => 'learn SQL', :list_id => 1})
       expect(task1).to(eq(task2))
     end
   end
 
   describe('#save') do
     it('adds task to the array of saved tasks') do
-      test_task = Task.new({:description => 'learn SQL'})
+      test_task = Task.new({:description => 'learn SQL', :list_id => 1})
       test_task.save()
       expect(Task.all()).to(eq([test_task]))
     end
@@ -30,14 +20,21 @@ describe(Task) do
 
   describe('#description') do
     it('lets you add give a description of a task') do
-      test_task = Task.new('put your pants on')
-      expect(test_task.description()).to(eq('put your pants on'))
+      test_task = Task.new({:description => 'learn SQL', :list_id => 1})
+      expect(test_task.description()).to(eq('learn SQL'))
     end
   end
 
   describe('.all') do
     it('is empty at first') do
       expect(Task.all()).to(eq([]))
+    end
+  end
+
+  describe("#list_id") do
+    it("lets you read the list ID out") do
+      test_task = Task.new({:description => "learn SQL", :list_id => 1})
+      expect(test_task.list_id()).to(eq(1))
     end
   end
 
@@ -49,11 +46,11 @@ describe(Task) do
   #   end
   # end
 
-  describe('.clear') do
-    it('empties out all of the saved tasks') do
-      Task.new("put your socks on").save()
-      Task.clear()
-      expect(Task.all()).to(eq([]))
-    end
-  end
+  # describe('.clear') do
+  #   it('empties out all of the saved tasks') do
+  #     Task.new({:description => 'learn SQL'}).save()
+  #     Task.clear()
+  #     expect(Task.all()).to(eq([]))
+  #   end
+  # end
 end
